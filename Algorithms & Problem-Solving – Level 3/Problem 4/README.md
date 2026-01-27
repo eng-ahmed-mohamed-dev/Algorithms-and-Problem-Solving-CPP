31546a4f9cc70c0738ab5886f21205b4c335e46e
Eng Ahmed Mohamed <eng.ahmed.mohamed.dev@gmail.com>
2026-01-27 10:12:45 +0200

    Solve Problem 4: calculate the sum of each column in a 3x3 matrix

---
+++ Algorithms & Problem-Solving – Level 3/Problem 4/README.md
@@ -0,0 +1,20 @@
+# Problem 4 – Sum of Each Column in a 3x3 Matrix
+
+## Description
+In this problem, a 3x3 matrix is filled with random numbers, then the sum of each column is calculated and printed.
+
+## Requirements
+- Create a 3x3 matrix
+- Fill the matrix with random numbers
+- Print the matrix
+- Calculate the sum of each column
+- Display each column sum
+
+## Concepts Used
+- Arrays
+- Loops
+- Functions
+- Random numbers
+
+## Language
+- C++
---
+++ Algorithms & Problem-Solving – Level 3/Problem 4/Sum Each Col in Matrix.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+
+ using namespace std;
+
+int RandomNumber(int From, int To)
+{
+	int randNum = rand() % (To - From + 1) + From;
+	return randNum;
+}
+
+void FillMatrixWithRandomNumbers(int arr[3][3], short Rows, short Columns)
+{
+	for (short i = 0; i < Rows; i++)
+	{
+		for (short j = 0; j < Columns; j++)
+		{
+			arr[i][j] = RandomNumber(1, 100);
+		}
+	}
+}
+
+void PrintMatrix(int arr[3][3], short Rows, short Columns)
+{
+	for (short i = 0; i < Rows; i++)
+	{
+		for (short j = 0; j < Columns; j++)
+		{
+			cout << setw(3) << arr[i][j] << "\t";
+		}
+
+		cout << endl;
+	}
+}
+
+short ColumnSum(int arr[3][3], short Rows, short ColumnNumber)
+{
+	short Sum = 0;
+
+	for (short i = 0; i <= Rows -1; i++)
+	{
+		Sum += arr[i][ColumnNumber];
+	}
+
+	return Sum;
+}
+
+void PrintEachColumnsSum(int arr[3][3], short Rows, short Columns)
+{
+	cout << "\nThe Following are the sum of each col in the matrix: \n";
+	
+	for (short j = 0; j < Columns; j++)
+	{
+		cout << " Col " << j + 1 << " Sum = " << ColumnSum(arr, Rows, j) << endl;
+	}
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	int arr[3][3];
+
+	FillMatrixWithRandomNumbers(arr, 3, 3);
+
+	cout << "The Following is a 3*3 random matrix: \n";
+	PrintMatrix(arr, 3, 3);
+
+	PrintEachColumnsSum(arr, 3, 3);
+
+	return 0;
+}
